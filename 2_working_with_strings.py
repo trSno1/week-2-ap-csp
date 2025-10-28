0531995313612d89686a1c5f8d1aa1644b289c97
trSno1 <aguilarjayden93@gmail.com>
2025-10-28 19:58:35 +0000

    edits

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -31,7 +31,10 @@ name = "Jayden Aguilar"
 print("Is Uppercase?:", phrase.isupper())
 print("name:", name.islower())
 print("name", name.capitalize)
+print("Length of phrases:"len(phrase))
 
+declaration_of_independence = "When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation."
+length_dec = len(declaration_of_independence) 
 # # Check if all characters are uppercase
 # print("Is Uppercase?", phrase.isupper())  # Output: False
 
