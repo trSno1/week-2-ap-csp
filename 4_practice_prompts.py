6128f81f16899dae4dd6ac447aa6456fc6c50e7e
trSno1 <aguilarjayden93@gmail.com>
2025-11-07 15:34:32 +0000

    Practice 4

--- 4_practice_prompts.py
+++ 4_practice_prompts.py
@@ -20,15 +20,28 @@ print(500 + 55)
 ##############################################################################################################
 # Find 3 objects around the room and create variables from it,
 # Insert those variables into an f-string sentence(look at slide 22)in repl.it
-
-
-# Familiarize yourself with the syntax of the print() function.
+item1 = "Keyboard"
+item2 = "Mouse"
+item3 = "Bookbag"
+message = f"There is three items in our computer room. The items are {item1}, {item2} and {item3}."
+print(message)
+# Familiarize yourself with the 
+# syntax of the print() function.
 # Print your name.
 # Print today's date.
 # Print the name of your favorite movie.
+name = "Jayden"
+print(name)
+date = "11/7/25"
+print(date)
+movie_name = "Pursuit of Happyness"
+print(movie_name)
 
 # Print your name and age on separate lines using a single print() function.
 # Use f-strings to print a message like: "In 10 years, [Your Name] will be [Your Age + 10] years old."
+age = 17
+print(f"Name: {name}/nAge: {age}")
+print(f"In 10 years, {name} will be {age+10} years old.")
 
 ##############################################################################################################
 
@@ -56,7 +69,18 @@ print(500 + 55)
 # string index the word beetle and print it out
 # print the last word of the summary
 # print the summary in reverse
+blue_beetle_summary = "Jaime Reyes suddenly finds himself in possession of an ancient relic of alien biotechnology called the Scarab. When the Scarab chooses Jaime to be its symbiotic host, he's bestowed with an incredible suit of armor that's capable of extraordinary and unpredictable powers, forever changing his destiny as he becomes the superhero Blue Beetle."
+print(len(blue_beetle_summary))
+print("Uppercase:", blue_beetle_summary.upper())
+print(blue_beetle_summary)
+print("Lowercase:", blue_beetle_summary.lower())
+new_text = blue_beetle_summary.replace("blue","red")
 
+print(new_text)
+print("Beetle")
+blue_beetle_summary = "Jaime Reyes suddenly finds himself in possession of an ancient relic of alien biotechnology called the Scarab. When the Scarab chooses Jaime to be its symbiotic host, he's bestowed with an incredible suit of armor that's capable of extraordinary and unpredictable powers, forever changing his destiny as he becomes the superhero Blue Beetle."
+reversed_string = blue_beetle_summary[::-1]
+print(reversed_string)
 
 ##########################input practice#############################################
 #input is when we ask the user for input/data
@@ -66,18 +90,25 @@ print(500 + 55)
 # Write Python code that allows the user to enter their answer, by making them the following question:
 # What are you learning today?
 # Your code must be able to print to the screen whatever is entered by the user (use the print function).
+learn = input("What are you learning today?")
+print(f"You're learning {learn} today.")
 
 # Input Practice #2
 # Write Python code that allows the user to enter their answer, by making them the following question:
 # Where are you from?
 # Your code must be able to print to the screen whatever is entered by the user (use the print function).
+Location = input("Where are you from?")
+print(f"You are from {location}")
+
 
 # Input Practice #3
 # Write Python code that displays the user's full name on the screen, by allowing them to enter their first and last name with the following instructions:
 # What is your name?
 # What is your surname?
 # The code must be able to print the user's first and last name on the screen, separated by a space.
-
+first_name = input("What is your first name?")
+Last_name = input("What is your surname?")
+Print(f"Your name is {first_name} {last_name}")
 # Exercise:
 # Write a program that asks the user for their name and favorite color, then prints a message using both pieces of information.
 
